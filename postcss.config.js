9e80261a084915756146ca0d462b04b568781939
hadidi09 <cheikhhadramy@yahoo.fr>
2021-09-16 21:53:26 +0200

    Revert " solve postcss error"
    This reverts commit 8391c527c406844ecaba3200e3e3202393b0be2b.

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   plugins: [
     "tailwindcss",
-    "autoprefixer",
+    "autopprefixer",
     [
       "@fullhuman/postcss-purgecss",
       process.env.NODE_ENV === "production"
