d9f57a91e0d956f485a372c8ae3d79706f5a9d34
hadidi09 <cheikhhadramy@yahoo.fr>
2021-09-16 22:46:41 +0200

    unused css 1.1

--- postcss.config.js
+++ postcss.config.js
@@ -1,18 +1,30 @@
-const purgecss = require('@fullhuman/postcss-purgecss')({
-  // Specify the paths to all of the template files in your project
-  content: ['./pages/**/*.js', './components/**/*.js'],
-
-  // make sure css reset isnt removed on html and body
-  whitelist: ['html', 'body'],
-
-  // Include any special characters you're using in this regular expression
-  defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
-});
-
 module.exports = {
-  plugins: [
-    require('tailwindcss'),
-    require('autoprefixer'),
-    ...(process.env.NODE_ENV === 'production' ? [purgecss] : [])
+  "plugins": [
+    "tailwindcss",
+    "autoprefixer",
+    "postcss-flexbugs-fixes",
+    [
+      "postcss-preset-env",
+      {
+        "autoprefixer": {
+          "flexbox": "no-2009"
+        },
+        "stage": 3,
+        "features": {
+          "custom-properties": false
+        }
+      }
+    ],
+    [
+      '@fullhuman/postcss-purgecss',
+      {
+        content: [
+            './pages/**/*.{js,jsx,ts,tsx}',
+            './components/**/*.{js,jsx,ts,tsx}'
+        ],
+        defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
+        safelist: ["html", "body"]
+      }
+    ],
   ]
-};
+}
