8c705c62a255b6efd62d773cee38280bac2144cc
hadidi09 <cheikhhadramy@yahoo.fr>
2021-12-14 17:24:58 +0100

    remove dynamic

--- pages/index.js
+++ pages/index.js
@@ -6,31 +6,16 @@ import Description from "./components/description";
 import Layout from "./components/Layout";
 import Projets from "./components/projets";
 import Resume from "./components/resume";
-import dynamic from 'next/dynamic'
-
-const DynamicLazyComponentDescription= dynamic(() => import('./components/description'), { ssr : false
-  
-})
-
-const DynamicLazyComponentCompetences = dynamic(() => import('./components/competences'), { ssr : false
-  
-})
-const DynamicLazyComponentProjets = dynamic(() => import('./components/projets'), { ssr : false
-  
-})
-const DynamicLazyComponentResume = dynamic(() => import('./components/resume'), { ssr : false
-  
-})
 
 
 export default function Home() {
   return (
    
     <Layout   page="ndiaye cheikh el hadramy">
-      <DynamicLazyComponentDescription />
-      <DynamicLazyComponentCompetences />
-      <DynamicLazyComponentProjets />
-      <DynamicLazyComponentResume />
+      <Description />
+      <Competences />
+      <Projets />
+      <Resume />
     </Layout>
    
   );
